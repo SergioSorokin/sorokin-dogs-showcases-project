8d79029a5d8d6e8c32683193c47719c2867a8687
Sergei Sorokin <sergei.sorokin@itomy.ch>
2021-07-05 17:43:49 +0300

    add screen "breed_list_screen"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/breed_list_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -8,13 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         //
 
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: BreedDogs(),
     );
   }
 }
